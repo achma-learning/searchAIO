40a063bf156fa74e12eb4ab7c260a0efa73d1a0a
achma-learning <139859609+achma-learning@users.noreply.github.com>
2026-02-19 21:57:26 +0000

    upade

--- BookmarkOpener/bg.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
-  if (request.action === "openBookmarks" && request.query) {
-    chrome.tabs.create({ url: `chrome://bookmarks/?q=${request.query}` });
-  }
-});
